63fd3a81c2077fa5594f2d3daadd8191b068ac32
Przemysław Sitek <przemyslaw.sitek@gmail.com>
2018-08-27 21:21:55 +0200

    TestHelper: Add support  for using QStrings in CppUnit assertions

--- src/tests/test_helper.h
+++ src/tests/test_helper.h
@@ -3,6 +3,7 @@
 
 #include <QString>
 #include <cassert>
+#include <cppunit/TestAssert.h>
 
 class TestHelper {
 	static TestHelper *	__instance;
@@ -17,6 +18,23 @@ class TestHelper {
 	static void			create_instance();
 	static TestHelper* get_instance() { assert(__instance); return __instance; }
 };
+
+
+/* Enable printing QStrins in CppUnit */
+template<>
+struct CppUnit::assertion_traits<QString>
+{
+	static bool equal( const QString &lhs, const QString &rhs)
+	{
+		return lhs == rhs;
+	}
+
+	static std::string toString( const QString &str )
+	{
+		return str.toStdString();
+	}
+};
+
 
 #define H2TEST_FILE(name) TestHelper::get_instance()->test_file(name)
 
